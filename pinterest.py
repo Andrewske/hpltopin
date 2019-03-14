f8f78ff6f870b1f3c5c5763027e4bf6e40727a84
Kevin <kevin@bonanza.com>
2019-03-14 13:24:13 -0700

    organizing project

--- pinterest.py
+++ pinterest.py
@@ -3,31 +3,43 @@ import json
 import requests
 
 
-class Pinterest():
+class Pinterest(self):
     def __init__(self):
         self.api_url = 'https://api.pinterest.com/v1/pins/'
 
-    def post_item_to_pinterest(self):
+    def post_item_to_pinterest(self, listing, pinterest_username, board_name):
 
-        headers = {
-            'Content-Type' : 'application/json; charset=utf-8'
-        }
-        
-        username = 'kevinbigfoot'
-        boardname = 'test'
 
-        listing = {'title': "MVMT Men's Watches | Blacktop Collection | Starlight Black | 47mm | SALE", 'price': '170.0', 'itemURL': 'https://www.bonanza.com/listings/MVMT-Men-s-Watches-Blacktop-Collection-Starlight-Black-47mm-SALE/644198149', 'imageURL': 'https://images.bonanzastatic.com/afu/images/42ea/9902/42cb_6741852984/star1.jpg'}
+        listing = {
+            'title': 'MVMT Men\'s Watches | Blacktop Collection | Starlight Black | 47mm | SALE',
+            'price': '170.0',
+            'itemURL': 'https://www.bonanza.com/listings/MVMT-Men-s-Watches-Blacktop-Collection-Starlight-Black-47mm-SALE/644198149',
+            'imageURL': 'https://images.bonanzastatic.com/afu/images/42ea/9902/42cb_6741852984/star1.jpg'
+            }
 
-        item_dictionary = {'access_token' : secrets.pinterest_token, 'board': username + '/' + boardname, 'note': listing['title'], 'link': listing['itemURL'], 'image_url': listing['imageURL']}
+        item_dictionary = {
+            'access_token' : secrets.pinterest_token,
+            'board': self.pinterest_username + '/' + self.board_name,
+            'note': self.listing['price'] + ' ' + self.listing['title'],
+            'link': self.listing['itemURL'],
+            'image_url': self.listing['imageURL']
+            }
 
-        payload = json.dumps(item_dictionary)
 
-        response = requests.post(self.api_url,params=payload, headers=headers)
-
+        response = requests.post(self.api_url + 'pins/',params=item_dictionary)
         response_json = response.json()
+        print(response_json)
 
-        print(response)
+    def create_pinterest_board(self, title):
 
+        item_dictionary = {
+            'access_token' : secrets.pinterest_token,
+            'name' : title
+        }
+        response = requests.post(self.api_url + 'boards/',params=item_dictionary)
+        response_json = response.json()
+        print(response_json)
+        
 p = Pinterest()
 
 p.post_item_to_pinterest()
--- pinterest_hpl.py
+++ pinterest_hpl.py
@@ -2,9 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import secrets
 import time
-import pinterest_example
 import bonanza
+#import pinterest
 import urllib
+import re
 
 #Need to have Docker running
 #Postman for Pinterest API
@@ -18,72 +19,21 @@ pinterest_profile = "https://www.pinterest.com/"+secrets.pinterest_username+"/bo
 pinterest = "https://www.pinterest.com/"
 #Then we need to open this page
 driver = webdriver.Chrome()
-class Pinterest(object):
+
+class hpl_to_pinterest_board(self, hpl_url):
     def __init__(self):
-        self.http_timeout = 15
+        self.url = help_url
 
-
-    def pinterest_log_in():
-        driver.get(pinterest)
-        log_in_button = driver.find_element_by_link_text("Already a member? Log in")
-        log_in_button.click()
-        email = driver.find_element_by_id('email')
-        password = driver.find_element_by_id('password')
-        email.send_keys(secrets.pinterest_email)
-        password.send_keys(secrets.pinterest_password)
-        redButton = driver.find_element_by_class_name("SignupButton")
-        redButton.click()
-
-
-
-
-
-    #Take the title of the HPL and create a board
-    def create_hpl(url):
-        driver.get(url)
-        title = driver.title.strip("- Hand Picked List")
-        log_in()
-        driver.get(pinterest_profile)
-        create_board_button = driver.find_element_by_class_name("Jea gjz gpV mQ8 mix xuU zI7 iyn Hsu")
-        create_board_button.click()
-        name = driver.find_element_by_name("boardName")
-        name.send_keys(title)
-
-
-    #create_hpl(hpl_url)
-    listing_url = "https://www.bonanza.com/listings/Romain-Jerome-Titanic-DNA-Ultimate-Tourbillon-120-Hour-reserve-Limited-Ed-9/388627036"
-
-    #Function to create Pin
-    def create_pin(listing_url):
-        log_in()
-        create_pin_button = driver.find_element_by_link_text("Create Pin")
-        create_pin_button.click()
-        save_from_site_button = driver.find_element_by_link_text("Save from site")
-        save_from_site_button.click()
-        pin_draft_link = driver.find_element_by_id("pin-draft-link")
-        pin_draft_link.send_keys(listing_url)
-        choose_a_board_button = driver.find_element_by_link_text("Choose a board")
-        choose_a_board_button.click()
-        choose_current_board = driver.find_element_by_link_text(title)
-        choose_a_board_button.click()
-
-    #create_pin(listing_url)
-
-
-    def find_url():
-        driver.get(url)
+    def find_listings(self):
+        driver.get(self.url)
         listings = driver.find_elements_by_class_name("image_wrap")
-        for listing in listings:
-            listing_url = listing.get_attribute("href")
-            create_pin(listing_url)
+        new_list = []
+        for l in listings:
+            new_list.append(re.match('.*?([0-9]+)$', l))
+
         driver.close()
-    #For each item, create Pin
-
-    #create_hpl(hpl_url)
-    base = "https://api.pinterest.com/v1/"
-    note = "Diesel DZ7273 little daddy white gold dial white leather strap unisex watch"
-    link = "https://www.bonanza.com/listings/Diesel-DZ7273-little-daddy-white-gold-dial-white-leather-strap-unisex-watch/660154339"
-    board_name = "Watch Yourself"
+        print(new_list)
 
 
-    def post_to_pinterest():
+hpl = hpl_to_pinterest_board()
+hpl.find_listings(hpl_url)
