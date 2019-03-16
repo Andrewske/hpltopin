e06e98c54e2cd755697d2b4867bee9bf9e24c203
Kevin <kevin@bonanza.com>
2019-03-15 19:38:01 -0700

    create webpage

--- pinterest.py
+++ pinterest.py
@@ -1,12 +1,55 @@
 import secrets
 import json
 import requests
+import webbrowser
+import urllib.parse
+from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+app = Flask(__name__)
+app.debug = True
 
-api_url = 'https://api.pinterest.com/v1/'
+api_url = 'https://api.pinterest.com/'
 
+@app.route()
 def authenticate_user():
-    pass
+    auth_code_dict = {
+        'response_type' : 'code',
+        'redirect_uri' : 'https://andrewske.github.io/pinterest-bonanza-api/',
+        'client_id' : '5021381484636841344',
+        'scope' : ['read_public', 'write_public'],
+        'state' : 'style20276'
+    }
+
+    params = urllib.parse.urlencode(auth_code_dict)
+    url = api_url + 'oauth/?' + params
+
+
+    driver = webdriver.Chrome()
+
+    response = webbrowser.open(url)
+    if response:
+        print(driver.current_url())
+    else:
+        code = print(nope)
+
+    access_token = {
+        'grant_type' : 'authorization_code',
+        'client_id' : secrets.pinterest_app_id,
+        'client_secret' : secrets.pinterest_token,
+        'code' : code
+
+    }
+
+    print(code)
+    driver.close()
+    #response = requests.post(api_url + 'v1/oaut/token',params=access_token)
+
+    #response_json = response.json()
+    #print(response_json)
+    
+    
 
 
 def post_item_to_pinterest(listing, title):
@@ -24,7 +67,7 @@ def post_item_to_pinterest(listing, title):
         }
 
 
-    response = requests.post(api_url + 'pins/',params=item_dictionary)
+    response = requests.post(api_url + 'v1/pins/',params=item_dictionary)
 
     response_json = response.json()
     print(response_json)
@@ -35,6 +78,9 @@ def create_pinterest_board(title):
         'access_token' : secrets.pinterest_token,
         'name' : title
     }
-    response = requests.post(api_url + 'boards/',params=item_dictionary)
+    response = requests.post(api_url + 'v1/boards/',params=item_dictionary)
     response_json = response.json()
     print(response_json)
+
+if __name__ == '__main__':
+    app.run(debug = True)
